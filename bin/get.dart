de3dd4e46e9b80cce3fb0150ee2659121b8015bc
Steve Bennett <stevebennett08@gmail.com>
2015-06-06 14:01:14 -0400

    added fix to handle extracting dirs in archives

--- bin/get.dart
+++ bin/get.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:archive/archive.dart';
 import 'src/utils.dart';
 
-main([List<String> args]) {  
+main([List<String> args]) {
   configureWithArgs(args);
   downloadShell().then((_) {
     unzipShell();
@@ -16,8 +16,8 @@ Future downloadShell() {
     return new Future(() => print('Found in cache.'));
   }
   new Directory('.cache').createSync(recursive: true);
-  
-  String url = 
+
+  String url =
       'https://github.com/atom/electron/releases/download/v${version}/electron-v${version}-${os}-${arch}.zip';
 
   print('Downloading electron..');
@@ -28,24 +28,49 @@ Future downloadShell() {
 }
 
 unzipShell() {
-  if (new Directory('.cache/electron-v${version}-${os}-${arch}').existsSync()) {
+  String out = '.cache/electron-v${version}-${os}-${arch}';
+
+  if (new Directory(out).existsSync()) {
+    print('Electron already unzipped.  Skipping.');
     return;
   }
-  
+
+  // Read the Zip file from disk.
+  File zip = new File('$out.zip');
+  List<int> bytes = zip.readAsBytesSync();
+
   print('Extracting files (This takes a while)..');
-  File archive = new File('.cache/electron-v${version}-${os}-${arch}.zip');
-  List<int> bytes = archive.readAsBytesSync();
-  Archive runnerArchive;
-  
-  runnerArchive = new ZipDecoder().decodeBytes(bytes);
-  for (ArchiveFile file in runnerArchive) {
-    String filename = file.name;
-    List<int> data = file.content;
-    new File('.cache/electron-v${version}-${os}-${arch}/$filename')
-        ..createSync(recursive: true)
-        ..writeAsBytesSync(data);
+  Archive archive = new ZipDecoder().decodeBytes(bytes);
+  for (ArchiveFile item in archive) {
+    String name = item.name;
+    List<int> data = item.content;
+    // We need to check if current item is a file or directory.
+    if (!name.endsWith('/')) {
+      // Current item is a file.
+      File currentFile = new File('$out/$name');
+      try {
+        currentFile.createSync(recursive: true);
+        currentFile.writeAsBytesSync(data);
+      } catch (e) {
+        print('Error: $e');
+        print('Deleting intermediate data.');
+        new Directory(out).deleteSync(recursive: true);
+        exit(1);
+      }
+    } else {
+      // Current item a directory.
+      Directory currentDirectory = new Directory('$out/$name');
+      try {
+        currentDirectory.createSync(recursive: true);
+      } catch (e) {
+        print('Error: $e');
+        print('Deleting intermediate data.');
+        new Directory(out).deleteSync(recursive: true);
+        exit(1);
+      }
+    }
   }
-  
+
   if (os == LINUX || os == OSX){
     print('Updating file permissions.');
     Process.runSync('chmod', ['777', '.cache/electron-v${version}-${os}-${arch}/electron']);
